60728d50f5fbf8a6d1c4d7bf468431ce9e037371
ReeceCole <reececharlescole@gmail.com>
2022-07-12 15:47:27 -0500

    PROJECT2_FUNCTIONS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function() {
+    var str = "This is the buttton text!";
+    document.getElementById("Button_Text").innerHTML = str; 
+} //This function creates the click me button which upon being clicked displays this is a button of text//
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+} //This function concatenates to strings and displays them in the form of a button//
