0b04f0fe4f6a83c2b4f433f905e6842a4c24d163
ReeceCole <reececharlescole@gmail.com>
2022-07-12 15:27:22 -0500

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+ var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Heromine", Daughter = "Penny", Son ="Zoro"; //This code is setting variables//
+ document.write(Dad); //This writes dad//
+ var A = "\"Be who you are and say what you feel,"+" because those who don\'t matter and those who matter don\'t mind."+"-Dr. Seuss"; //this uses back slashes to correctly display a quote on the page//
+ document.write(A);//This actually displays the quote//
+ window.alert("Hello, world!");//alert when you open the browser//
+  document.write("Hello, world!");//writes hello world//
+ document.write(3 + 3); //writes 6//
+  function AlertBox() { 
+    alert("Wow!");       
+    } //this is a button used to displayw wow when clicked//
